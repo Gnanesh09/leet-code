cfa3706f9e774e610d1a3effcf4ebf45d7c06ca2
Gnanesh G <gnanesh9206@gmail.com>
2026-01-07 22:42:42 +0530

    66 added

---
+++ easy/575_distribute_candies.py
@@ -0,0 +1,14 @@
+candyType = [1,1,1,1]
+
+def divide(candyType):
+   
+    for i in candyType:
+        if i==i+1:
+            print (can = 1)
+               
+    # can = int(len(candyType)/2)
+    # return print(can)
+
+
+print(divide(candyType)
+)
